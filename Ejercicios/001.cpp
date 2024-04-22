36768825b8dd05d7d37b0e9f7bfaa75e0248200e
liocutro.cs <lionel.cutro@gmail.com>
2024-04-22 19:57:39 -0300

    Create 001.cpp
    ejercicio practica de vectores, objetos, clases

---
+++ Ejercicios/001.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Alumno {
+
+
+ int dni;
+ string nombre;
+ string mail;
+
+
+};
+
+int main () {
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}

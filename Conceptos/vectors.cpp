645f001298fc6afdf9335d1abc3d0f3bd8b8847f
liocutro.cs <lionel.cutro@gmail.com>
2024-04-22 19:43:22 -0300

    .
    .

---
+++ Conceptos/vectors.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main () {
+
+
+ vector <double> Vector_1;          // vector sin inicializar ni indicar tamaño
+
+//  Vector_1 = 10;                  error
+//  Vector_1 [0] = 1;               error 
+
+ vector <int> Vector_3 (10);        // (10) es el tamaño del vector
+
+
+
+
+
+}

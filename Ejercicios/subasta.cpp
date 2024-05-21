21dddd0f3af0b6e4d4eef14e0c3e44724489415f
liocutro.cs <lionel.cutro@gmail.com>
2024-05-21 18:46:20 -0300

    Create subasta.cpp
    add menu for start

---
+++ Ejercicios/subasta.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+void menu_ingreso ( string& nombre_p ) 
+
+{
+
+    cout << "Bienvenido a la Subasta" << endl;
+    cout << "Por favor ingrese su nombre" << endl;
+    cout << "-> ";
+    getline ( cin, nombre_p );
+
+
+};
+
+int main () {
+
+ string nombre_p; 
+
+
+
+
+
+    return 0;
+};
+

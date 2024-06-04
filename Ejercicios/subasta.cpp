bee1e4db216cc264a56767979b17472a804bb44a
liocutro.cs <lionel.cutro@gmail.com>
2024-06-04 18:33:40 -0300

    Update subasta.cpp
    switch to choose lot

--- Ejercicios/subasta.cpp
+++ Ejercicios/subasta.cpp
@@ -20,7 +20,7 @@ class Lote
     void printLotes () const 
     {
         for ( const auto& lote : lotes ) {
-            cout << "-> " << lote.nombreL << endl;
+            cout << "" << lote.nombreL << endl;
         }
     }
 
@@ -42,12 +42,35 @@ void menu_ingreso ( string& nombre_p )
 
 };
 
-void menu_lotes ( Lote& milote ) 
+void menu_lotes ( Lote& milote, int& resp_lote ) 
 
 {
 
     cout << "Esta es la lista de lotes disponibles para ofertar" << endl;
+    
     milote.printLotes ();
+
+    cout << "\nSeleccione a continuacion el lote que quiere ofertar" << endl;
+    cout << "-> ";
+    cin >> resp_lote;
+
+    switch ( resp_lote )
+    {
+    case 1:
+        cout << "\nHa seleccionado el lote 1 -> Computadora" << endl;
+    
+    break;
+    
+    case 2:
+        cout << "\nHa seleccionado el lote 2 -> Celular" << endl;
+    
+    break;
+
+    case 3:
+        cout << "\nHa seleccionado el lote 3 -> Jarron Chino" << endl;
+
+    break;
+    }
 
 };
 
@@ -55,14 +78,17 @@ int main () {
  
  Lote milote;
  
+ int resp_lote;
+ int resp_usr;
  string nombre_p; 
  string nombreL;
 
-    milote.addLote ( "Computadora" );
-    milote.addLote ( "Celular" );
-    milote.addLote ( "Jarron Chino" );
+    milote.addLote ( "\n1- Computadora" );
+    milote.addLote ( "2- Celular" );
+    milote.addLote ( "3- Jarron Chino" );
 
-    milote.printLotes();
+    menu_lotes ( milote, resp_lote);
+
 
     return 0;
 };

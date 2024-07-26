e4375a6e266bba466607e8aafb5cc0884e0e3e32
liocutro.cs <lionel.cutro@gmail.com>
2024-07-26 11:25:31 -0300

    Update subasta.h
    comments added

--- Trabajo Practico Subasta/subasta.h
+++ Trabajo Practico Subasta/subasta.h
@@ -1,21 +1,21 @@
 #ifndef SUBASTA_H
 #define SUBASTA_H
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include <fstream>
-#include <filesystem>
+#include <iostream>                         // biblioteca estandar de IO
+#include <vector>                           // biblioteca para manejar vectores
+#include <string>                           // biblioteca para manipular strings
+#include <fstream>                          // biblioteca para trabajar con archivos
+#include <filesystem>                       // biblioteca para trabajar con el sistema de archivos
 
 using namespace std;
 
 class Lote 
 {
- private:
+ private:                                   // estructura creada para almacenar estos datos de subasta
     struct loteInfo {
-        string nombreL;
-        string nombreOferente;
-        int ofertaActual = 0;
+        string nombreL;                     // nombre del lote asignado
+        string nombreOferente;              // nombre del participante
+        int ofertaActual = 0;               // variable con la oferta actual
     };
 
     vector <loteInfo> lotes;
@@ -24,21 +24,33 @@ class Lote
  public:
  
  Lote();
+ // funcion para leer el archivo con los lotes
  void leerLotesDeArchivo (const string& nombreArchivo );
+ 
+ // funcion para imprimir la lista de lotes desde el archivo
  void printLotes () const;
+ 
+ // funcion para que el participante haga la oferta deseada
  void realizarOferta ( int numeroLote, int montoOferta, const string& nomreOferente );
+ 
+ // funcion para guardar los resultados de la subasta creando un archivo
  void guardarResultadosEnArchivo ( const string& nombreArchivo ) const;
  int getLotesSize () const;
 };
 
-class ManipularArchivos
-{
- public:
- void escribirEnArchivo (const string& nombreArchivo, const string& contenido);
-};
+// class ManipularArchivos
+// {
+//  public:
+//  void escribirEnArchivo (const string& nombreArchivo, const string& contenido);
+// };
 
+// funcion para el primer menu de usuario
 void menu_ingreso ( string& nombre_p );
+
+// funcion para la seleccion de lotes
 void menu_lotes ( Lote& milote, int& resp_lote );
+
+// funcion para solicitar el monto al usuario
 void solicitarOferta (Lote& milote, int& resp_lote, int& montoOferta, const string& nombreOferente );
 
 #endif

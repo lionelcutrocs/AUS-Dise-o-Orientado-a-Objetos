12c3974f96ef579ec080f72f4c2deb88c2805c45
liocutro.cs <lionel.cutro@gmail.com>
2024-08-02 19:43:55 -0300

    Update cache.h
    adding comments and correcting syntax

--- TP-Cache Manager/cache.h
+++ TP-Cache Manager/cache.h
@@ -14,55 +14,63 @@ using namespace std;
 
 template <class T>
 class CacheManager {
+
 private:
     int capacidad;                                                      // la capacidad de la cache
 
     // estructura que almacena los datos en la cache, T = obj almacenado y Iterator = iterador a la lista LRU
     map<string, pair<T, typename list<string>::iterator>> cache_data;
     list<string> lru_list;                                              // lista para mantener el orden del uso reciente LRU de claves
-    string filename = "cache_file.txt";                                 // destino del resultado de la cache
+    string nom_archivo = "cache_file.txt";                              // destino del resultado de la cache
 
-    bool escribir_arch(const string& key, const T& obj);
-    bool leer_arch(const string& key, T &obj);
-    void actualizar_lru(const string &key);
+    // metodos privados
+
+    bool escribir_arch(const string& key, const T& obj);                // funcion para escribir el obj en archivo
+    bool leer_arch(const string& key, T &obj);                          // funcion para leer el obj en archivo
+    void actualizar_lru(const string &key);                             // funcion para actualizar la lista LRU con la clave dada
 
 public:
-    CacheManager(int cap);
-    ~CacheManager();
+    CacheManager(int cap);                                              // constructor
+    ~CacheManager();                                                    // destructor
 
-    void insert(const string& key, const T& obj);
-    T get(const string& key);
-    void show_cache();
+    void insert(const string& key, const T& obj);                       // incerta un objeto en la cache
+    T get(const string& key);                                           // obtengo un objeto en la cache
+    void ver_cache();                                                   // muestra el contenido de la cache 
 };
 
 template <class T>
-CacheManager<T>::CacheManager(int cap) : capacidad(cap) {}
+CacheManager<T>::CacheManager(int cap) : capacidad(cap) {}              // inicializa la capacidad de la cache
 
 template <class T>
 CacheManager<T>::~CacheManager() {}
 
 template <class T>
 bool CacheManager<T>::escribir_arch(const string& key, const T& obj) {
-    ofstream ofs(filename, ios::app);
-    if (!ofs.is_open()) return false;
+    ofstream ofs(nom_archivo, ios::app);                                // apertura del archivo
+    if (!ofs.is_open()) return false;                                   // false por operacion sin exito
 
     ofs << key << " " << obj.id << " " << obj.value << " " << obj.data << endl;
     ofs.close();
-    return true;
+    return true;                                                        // true por operacion exitosa
 }
 
 template <class T>
-bool CacheManager<T>::leer_arch(const string& key, T &obj) {
-    ifstream ifs(filename);
-    if (!ifs.is_open()) return false;
 
+
+
+bool CacheManager<T>::leer_arch(const string& key, T &obj) 
+{
     string file_key;
-    while (ifs >> file_key) {
+
+    ifstream ifs(nom_archivo);                             // apertura del archivo indicado por nom_archivo
+    if (!ifs.is_open()) return false;                      // devuelve un false si no logra abrirse
+
+    while (ifs >> file_key) {                              // se lee la primera palabra "clave" y se almacena en file_key
         if (file_key == key) {
             int id, value;
             string data;
             ifs >> id >> value;
-            getline(ifs, data); // lee el resto de la línea como datos
+            getline(ifs, data);                            // lee el resto como datos
 
             obj = T(id, value, data);
             ifs.close();
@@ -114,7 +122,7 @@ T CacheManager<T>::get(const string& key) {
 }
 
 template <class T>
-void CacheManager<T>::show_cache() {
+void CacheManager<T>::ver_cache() {
     for (const auto &item : cache_data) {
         cout << item.first << " - " << item.second.first.id << ", " << item.second.first.value << ", " << item.second.first.data << endl;
     }
